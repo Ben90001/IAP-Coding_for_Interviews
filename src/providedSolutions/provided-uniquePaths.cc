c6c6fd60c6faaf2760c731c45c75c211cbf5ebf3
Ben <benhorn@gmx.net>
2024-08-26 15:37:44 +0200

    indexchange and other minor changes to uniquePaths_DP implementation

--- src/providedSolutions/provided-uniquePaths.cc
+++ src/providedSolutions/provided-uniquePaths.cc
@@ -5,11 +5,12 @@
 namespace providedSolutions{
 
     long uniquePaths_iter(int m, int n){
-        //check preconditions
+        // check preconditions
         if(!(1<=m && m<=30) || !(1<=n && n<=30)) throw std::out_of_range("Input out of bounds!");
 
-        //compute result
-        //following code has been taken from here: https://leetcode.com/problems/unique-paths/solutions/5423442/o-min-m-n-time-complexicity-solution-using-maths-c-solution/?envType=study-plan-v2&envId=dynamic-programming
+        // compute result
+        // the following code has been taken from here: 
+        // https://leetcode.com/problems/unique-paths/solutions/5423442/o-min-m-n-time-complexicity-solution-using-maths-c-solution/?envType=study-plan-v2&envId=dynamic-programming
         long long ans = 1;
         int x = std::max(m-1, n-1);
         int y = std::min(m-1, n-1);
@@ -23,36 +24,47 @@ namespace providedSolutions{
     }
 
     long uniquePaths_DP_recursion(int m, int n, long **memo){
-        if(memo[m][n]==-1){
-            memo[m-1][n]= uniquePaths_DP_recursion(m-1,n,memo);
-            memo[m][n-1]= uniquePaths_DP_recursion(m,n-1,memo);
-            return memo[m][n] = memo[m-1][n] + memo[m][n-1];
+        // recursive part: uncomputed case
+        if(memo[m-1][n-1]==-1){
+            return memo[m-1][n-1] = uniquePaths_DP_recursion(m-1,n,memo) \
+                                + uniquePaths_DP_recursion(m,n-1,memo);
         }
-        else return memo[m][n];
+        // use of memoization: precomputed case
+        else return memo[m-1][n-1];
     }
     long uniquePaths_DP(int m,int n){
-        //check whether n and m are in bound
-        if(!(1<=m && m<=30)) throw std::out_of_range("m is out of bounds!");
-        if(!(1<=n && n<=30)) throw std::out_of_range("n is out of bounds!");
+        // check preconditions
+        if(!(1<=m && m<=30) || !(1<=n && n<=30)) \
+            throw std::out_of_range("m and/or n is out of bounds!");
 
-        //basecase
+        // assert m,n > 1
         if(m==1 || n==1) return 1;
 
-        //define matrix
-        long* storage_ptr=new long[(m+1)*(n+1)];    //allocate at once for cache efficiency
-        //create accesser
-        long** memo=new long*[m+1];
-        for(int i=0;i<m+1;i++)memo[i]=storage_ptr + (n+1)*i;
+        // allocate matrix at once for cache efficiency
+        long* storage_ptr=new long[m*n];
 
-        //initialize matrix with -1 except for basecases
-        for(int i=0; i<=m; i++){
-            for(int j=0; j<=n; j++) {
-                if (i==1||j==1) memo[i][j] = 1;     //basecase
-                else memo[i][j] = -1;               //uncalculated
+        // create accessor
+        long** memo=new long*[m];
+        for(int i=0;i<m;i++)memo[i]=storage_ptr + n*i;
+
+        // initialize matrix
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++) {
+                // base case
+                if (i==0||j==0) memo[i][j] = 1; 
+                // mark uncomputed   
+                else memo[i][j] = -1;               
             }
         }
+
+        // calculate result
+        long result = uniquePaths_DP_recursion(m,n,memo);
         
-        return uniquePaths_DP_recursion(m,n,memo);
+        // clean up allocated memory
+        delete[] storage_ptr;
+        delete[] memo;
+
+        return result;
     }
 
     long uniquePaths_naive(int m, int n){
